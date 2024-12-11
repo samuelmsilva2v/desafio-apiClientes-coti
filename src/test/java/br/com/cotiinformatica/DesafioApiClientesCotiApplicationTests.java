6df8bcab4a00ddaa5dc5a50f182dbc03bf376964
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-11 14:41:20 -0300

    atualizarClienteTest

--- src/test/java/br/com/cotiinformatica/DesafioApiClientesCotiApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/DesafioApiClientesCotiApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.StandardCharsets;
@@ -31,73 +32,103 @@ import br.com.cotiinformatica.domain.models.dtos.EnderecoRequestDto;
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DesafioApiClientesCotiApplicationTests {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	private static UUID id;
 
 	@Test
 	@Order(1)
 	void cadastrarClienteTest() throws Exception {
-		
+
 		// Instanciando a biblioteca JavaFaker
 		var faker = new Faker(Locale.forLanguageTag("pt-BR"));
-		
-		/*
-		 * Preenchendo os dados para cadastrar o cliente
-		 */
+
+		// Preenchendo os dados para cadastrar o cliente
 		var request = new ClienteRequestDto();
 		request.setNome(faker.name().fullName());
 		request.setEmail(faker.internet().emailAddress());
 		request.setCpf(faker.number().digits(11));
 		request.setDataNascimento(faker.date().birthday());
-		
+
 		var endereco = new EnderecoRequestDto();
 		endereco.setLogradouro(faker.address().streetName());
+		endereco.setComplemento(faker.address().secondaryAddress());
 		endereco.setNumero(faker.address().buildingNumber());
 		endereco.setBairro(faker.address().city());
 		endereco.setCidade(faker.address().cityName());
 		endereco.setUf("RJ");
 		endereco.setCep(faker.number().digits(8));
-		
+
 		request.setEnderecos(List.of(endereco));
-		
+
 		// Fazendo a requisição para o serviço do API
-		var result = mockMvc.perform(post("/api/clientes")
-				.contentType("application/json")
-				.content(objectMapper.writeValueAsString(request)))
-				.andExpect(status().isOk())
-				.andReturn();
-		
-		/*
-		 * Capturando a mensagem devolvida pela API
-		 * após a realização do cadastro
-		 */
+		var result = mockMvc.perform(
+				post("/api/clientes").contentType("application/json").content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isOk()).andReturn();
+
+		// Capturando a mensagem devolvida pela API após a realização do cadastro
 		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
-		
-		// Ler os dados obtidos da API 
+
+		// Ler os dados obtidos da API
 		var response = objectMapper.readValue(content, ClienteResponseDto.class);
-		
-		/*
-		 * Verificando se os dados da resposta
-		 * coincidem com os da requisição
-		 */
+
+		// Verificando se os dados da resposta coincidem com os da requisição
 		assertNotNull(response);
 		assertNotNull(response.getId());
 		assertEquals(request.getNome(), response.getNome());
 		assertEquals(request.getEmail(), response.getEmail());
-		
+		assertEquals(request.getCpf(), response.getCpf());
+
+		// Armazenando o ID do produto cadastrado
 		id = response.getId();
 	}
 
 	@Test
 	@Order(2)
 	void atualizarClienteTest() throws Exception {
-		fail("Não implementado.");
+
+		// Instanciando a biblioteca JavaFaker
+		var faker = new Faker(Locale.forLanguageTag("pt-BR"));
+
+		// Preenchendo os dados para atualizar o cliente
+		var request = new ClienteRequestDto();
+		request.setNome(faker.name().fullName());
+		request.setEmail(faker.internet().emailAddress());
+		request.setCpf(faker.number().digits(11));
+		request.setDataNascimento(faker.date().birthday());
+
+		var endereco = new EnderecoRequestDto();
+		endereco.setLogradouro(faker.address().streetName());
+		endereco.setComplemento(faker.address().secondaryAddress());
+		endereco.setNumero(faker.address().buildingNumber());
+		endereco.setBairro(faker.address().city());
+		endereco.setCidade(faker.address().cityName());
+		endereco.setUf("SP");
+		endereco.setCep(faker.number().digits(8));
+
+		request.setEnderecos(List.of(endereco));
+
+		// Fazendo a requisição para o serviço do API
+		var result = mockMvc.perform(put("/api/clientes/" + id).contentType("application/json")
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isOk()).andReturn();
+
+		// Capturando a mensagem devolvida pela API após a realização do cadastro
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+		// Ler os dados obtidos da API
+		var response = objectMapper.readValue(content, ClienteResponseDto.class);
+
+		// Verificando se os dados da resposta coincidem com os da requisição
+		assertEquals(response.getId(), id);
+		assertNotNull(response.getNome());
+		assertNotNull(request.getNome(), response.getNome());
+		assertNotNull(request.getEmail(), response.getEmail());
+		assertNotNull(request.getCpf(), response.getCpf());
 	}
 
 	@Test
